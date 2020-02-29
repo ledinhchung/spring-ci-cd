4ef7811ab461b2ef4ff4bbb50c38153fc35bb5f0
Chung Le <ledinhchung.it@gmail.com>
2020-02-29 15:31:51 +0700

    Implement Unit test

--- src/test/java/com/chungle/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/chungle/demo/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.chungle.demo.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DemoApplication.class)
 class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mocMvc;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnDefaultMessage() throws Exception{
+		this.mocMvc.perform(get("/")).andExpect(content().string(containsString("Hello this is message from Demo 1 application")));
+	}
+
+	@Test
+	void shouldReturnHelloMessage() throws Exception {
+		this.mocMvc.perform(get("/test")).andExpect(content().string(containsString("Hello Chung 222")));
 	}
 
 }
